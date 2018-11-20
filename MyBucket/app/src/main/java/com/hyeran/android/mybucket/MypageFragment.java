74f275d2cc5d6bcb1b23ffde3ac4da404047c929
jiwon8578 <31886880+jiwon8578@users.noreply.github.com>
2018-11-21 00:27:56 +0900

    MyPageFragment
    MyPageFragment.java - 수정, 저장 버튼 구현.
    layout - 전반적으로 수정.
    기존에 있던 camera.png, pencil.png 를 새로운 파일로 바꾸고, saveimage.png 추가.

--- MyBucket/app/src/main/java/com/hyeran/android/mybucket/MypageFragment.java
+++ MyBucket/app/src/main/java/com/hyeran/android/mybucket/MypageFragment.java
@@ -2,16 +2,64 @@ package com.hyeran.android.mybucket;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 public class MypageFragment extends Fragment {
-
+    View view;
+    int count = 0;
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_mypage, container, false);
+        view = inflater.inflate(R.layout.fragment_mypage, container, false);
+
+        ImageButton modifyBtn = (ImageButton) view.findViewById(R.id.MypageModifyBtn);
+        ImageButton saveBtn = (ImageButton) view.findViewById(R.id.MyPageSaveBtn);
+
+        final ViewSwitcher switcher = (ViewSwitcher) view.findViewById(R.id.MyPageNameSwitcher);
+        final TextView nameTextView = (TextView) view.findViewById(R.id.MypageName);
+        final EditText nameEditText = (EditText) view.findViewById(R.id.MyPageNameEdit);
+
+        final ViewSwitcher switcher2 = (ViewSwitcher) view.findViewById(R.id.MyPageMottoSwitcher);
+        final TextView mottoTextView = (TextView) view.findViewById(R.id.MypageMotto);
+        final EditText mottoEditText = (EditText) view.findViewById(R.id.MyPageMottoEdit);
+
+        final ViewSwitcher switcher3 = (ViewSwitcher) view.findViewById(R.id.MyPageImageButtonSwticher);
+
+        modifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                nameEditText.setText(nameTextView.getText().toString());
+
+                mottoEditText.setText(mottoTextView.getText().toString());
+
+                if(count % 2 == 0) {switcher.showNext(); switcher2.showNext(); count++;}
+
+                switcher3.showNext();
+            }
+        });
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                nameTextView.setText(nameEditText.getText().toString());
+
+                mottoTextView.setText(mottoEditText.getText().toString());
+
+                if(count % 2 == 1) {switcher.showNext(); switcher2.showNext(); count++;}
+
+                switcher3.showNext();
+            }
+        });
         return view;
     }
 }
